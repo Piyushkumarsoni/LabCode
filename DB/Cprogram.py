17bdffecc116b73ddcc6251fb9d0313665c20dc8
Piyush Kumar <piyushkumar.aa@gmail.com>
2021-06-04 13:57:38 +0530

    some changes

--- DB/Cprogram.py
+++ DB/Cprogram.py
@@ -48,42 +48,14 @@ def Cpp_program(code,input_=[]):
     var4=mydir+"\\out.exe"
     subprocess.run([var1,var2,var3,var4])
     #x=subprocess.run(mydir+"\\out",stdout=PIPE)
-    try:
-        arg=""
-        for i in input_:
-            arg+=i
-        x=run(mydir+"\\out.exe", stdout=PIPE,
-            input=arg, encoding='ascii')
-        shutil.rmtree(mydir)
-        return x.stdout
-    except:
-        return "Error in code"
-# def Java_program(code,**args):
-#     fil=open("C:/Users/Sandip Das/Desktop/Online Exam/labcodes/DB/Javacode.java",'w')
-#     fil.write(code)
-#     var1="javac"
-#     var2="C:/Users/Sandip Das/Desktop/Online Exam/labcodes/DB/Javacode.java"
-#     var3="java"
-#     var4= "C:/Users/Sandip Das/Desktop/Online Exam/labcodes/DB/Javacode"
-
-#     subprocess.run([var1,var2])
-#     x=subprocess.run([var3,var4], stdout=PIPE)
-#     return x.stdout
-# def Java_program(code,input_=[]):
-#     fil=open("C:/Users/Sandip Das/Desktop/Online Exam/labcodes/DB/Javacode.java",'w')
-#     fil.write(code)
-#     fil.close()
-#     var1="javac"
-#     os.chdir("C:/Users/Sandip Das/Desktop/Online Exam/labcodes/DB/")
-#     var2="Javacode.java"
-#     var3="java"
-#     var4= "\"Javacode\""
-
-#     y=subprocess.run([var1,var2],stdout=PIPE)
-#     print(y.stdout)
-#     print("Compile done")
-#     x=subprocess.run([var3,var4], stdout=PIPE)
-#     return x.stdout
+    arg=""
+    for i in input_:
+        arg+=i
+    x=run(mydir+"\\out.exe", stdout=PIPE,
+        input=arg, encoding='ascii')
+    shutil.rmtree(mydir)
+    return x.stdout
+#
 def Java_program(code,input_=[]):
     # mydir=tempfile.mkdtemp(prefix="tempdir")
     mydir=base+str(uuid.uuid1())
@@ -101,7 +73,7 @@ def Java_program(code,input_=[]):
     var3="java"
     var4= "\"Javacode\""
     y=subprocess.run([var1,var2],stdout=PIPE)
-    subprocess.run([var3,var4],stdout=PIPE,stderr=PIPE)
+    #subprocess.run([var3,var4],stdout=PIPE,stderr=PIPE)
     # if y.returncode ==0:
     #     y=""
     # else:
@@ -113,33 +85,8 @@ def Java_program(code,input_=[]):
     try:
         x=run([var3,var4], stdout=PIPE,
             input=arg, encoding='ascii')
-        x=x.stdout.decode("utf-8")
+        x=x.stdout
     except:
         x="Error in the code"
     #shutil.rmtree(mydir)
-    return x+y
-
-#def python_program(code,input_=[]):
-#    mydir=base+str(uuid.uuid1())
-#    mkdir(mydir)
-#    var2=mydir+"\\pythoncode.py"
-#   (f,var2)=tempfile.mkstemp(prefix="pythontext",suffix=".py")
-#   f=open(var2,'w')
-#   f.write(code)
-#   f.close()
-#   var1="python"
-#   #var2="newpython.py"
-    #x=subprocess.run([var1,var2],stdout=PIPE)
-#   print(input_)
-#   arg=""
-#   for i in input_:
-#       arg+=i
-#   try:
-#       x=run([var1,var2], stdout=PIPE,
-#           input=arg, encoding='ascii')
-#       shutil.rmtree(mydir)
-#       return x.stdout
-#   except:
-#       return "Error in code"
-
-
+    return x
